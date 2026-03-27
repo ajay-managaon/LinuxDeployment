daed07f2162f206f24692b5aa0b4ab8a3edd3737
Ajay Managaon <ajaymanagaon@Mac.lan>
2026-03-27 10:46:13 +0530

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LinuxDeployment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
