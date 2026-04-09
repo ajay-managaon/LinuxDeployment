9fd7bd34afd176b95cb7e07dffd761d554dd61f4
Ajay Managaon <ajaymanagaon@gmail.com>
2026-04-09 19:58:28 +0530

    adding home controller changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,19 +6,32 @@ namespace LinuxDeployment.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
     public IActionResult Index()
     {
+        _logger.LogInformation("Index page loaded");
         return View();
     }
 
     public IActionResult Privacy()
     {
+        _logger.LogInformation("Privacy page loaded");
         return View();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        var requestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+
+        _logger.LogError("Error page shown. RequestId: {RequestId}", requestId);
+
+        return View(new ErrorViewModel { RequestId = requestId });
     }
 }
