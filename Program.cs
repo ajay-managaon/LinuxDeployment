672ca19cf503f1f3b7992cbca9c3131bd0e77441
Ajay Managaon <ajaymanagaon@gmail.com>
2026-04-09 20:44:23 +0530

    Updated program.cs

--- Program.cs
+++ Program.cs
@@ -1,46 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddApplicationInsightsTelemetry();
 
-// Create logger
-var logger = LoggerFactory
-    .Create(logging =>
-    {
-        logging.AddConsole();
-    })
-    .CreateLogger("Program");
-
-logger.LogInformation("Ajay Managaon Application starting...");
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddApplicationInsights();
+builder.Logging.SetMinimumLevel(LogLevel.Information);
 
 var app = builder.Build();
 
-logger.LogInformation("Application built successfully");
+var logger = app.Logger;
+logger.LogInformation("Ajay Managaon - App Starting");
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    logger.LogInformation("Running in Production environment");
-
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseRouting();
-
-logger.LogInformation("Middleware configured");
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-logger.LogInformation("Application configured. Starting app...");
+logger.LogInformation("Ajay Managaon - App Running");
 
 app.Run();
