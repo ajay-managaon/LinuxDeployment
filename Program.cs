bc93bc3cadd61e8d3976ec567fc257c1cb21de69
Ajay Managaon <ajaymanagaon@gmail.com>
2026-04-09 20:48:20 +0530

    Correcting Program,cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddApplicationInsightsTelemetry();
 
@@ -8,24 +9,42 @@ builder.Logging.AddConsole();
 builder.Logging.AddApplicationInsights();
 builder.Logging.SetMinimumLevel(LogLevel.Information);
 
+// Create logger
+var logger = LoggerFactory
+    .Create(logging =>
+    {
+        logging.AddConsole();
+    })
+    .CreateLogger("Program");
+
+logger.LogInformation("Ajay Managaon Application starting...");
+
 var app = builder.Build();
 
-var logger = app.Logger;
-logger.LogInformation("Ajay Managaon - App Starting");
+logger.LogInformation("Application built successfully");
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    logger.LogInformation("Running in Production environment");
+
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
 app.UseRouting();
+
+logger.LogInformation("Middleware configured");
+
 app.UseAuthorization();
+
+app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
 
-logger.LogInformation("Ajay Managaon - App Running");
+logger.LogInformation("Application configured. Starting app...");
 
 app.Run();
